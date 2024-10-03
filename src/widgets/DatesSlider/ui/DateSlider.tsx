856eea7d6f77c03796cd380e89cfcf8e1222fd2c
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-10-03 18:15:45 +0300

    started making adaptive

--- src/widgets/DatesSlider/ui/DateSlider.tsx
+++ src/widgets/DatesSlider/ui/DateSlider.tsx
@@ -3,33 +3,46 @@ import style from "./DateSlider.module.scss";
 import useDateStore from "entities/model/DateSlice";
 import { TEXT_FOR_DATE_SLIDES } from "shared/const/TextForDateSlides";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, FreeMode } from "swiper/modules";
+import { Navigation, FreeMode, Pagination } from "swiper/modules";
 import NextButtonIcon from "shared/assets/NextButton.svg";
 import "swiper/css";
+import "swiper/css/pagination";
 import classNames from "classnames";
+import { useMediaQuery } from "react-responsive";
 
 const DateSlider = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1000px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 750px)" });
   const current = useDateStore((state) => state.currentDateId);
   return (
     <div className={style.DateSlider}>
-      <div
-        id="prevButton"
-        className={classNames(style.SliderButton, style.prev)}
-      >
-        <NextButtonIcon />
-      </div>
+      {!isMobile && (
+        <div
+          id="prevButton"
+          className={classNames(style.SliderButton, style.prev)}
+        >
+          <NextButtonIcon />
+        </div>
+      )}
       <Swiper
         className={style.dates}
-        slidesPerView={3}
+        slidesPerView={isTabletOrMobile ? 2 : 3}
         spaceBetween={74}
         freeMode={true}
         direction="horizontal"
-        navigation={{
-          nextEl: "#nextButton",
-          prevEl: "#prevButton",
-          disabledClass: `${style.swiperButtonDisable}`,
-        }}
-        modules={[Navigation, FreeMode]}
+        navigation={
+          !isMobile && {
+            nextEl: "#nextButton",
+            prevEl: "#prevButton",
+            disabledClass: `${style.swiperButtonDisable}`,
+          }
+        }
+        pagination={
+          isMobile && {
+            clickable: true,
+          }
+        }
+        modules={[Navigation, FreeMode, Pagination]}
       >
         {TEXT_FOR_DATE_SLIDES[current].map((dateItem, id) => (
           <SwiperSlide key={id}>
@@ -37,9 +50,11 @@ const DateSlider = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div id="nextButton" className={style.SliderButton}>
-        <NextButtonIcon />
-      </div>
+      {!isMobile && (
+        <div id="nextButton" className={style.SliderButton}>
+          <NextButtonIcon />
+        </div>
+      )}
     </div>
   );
 };
